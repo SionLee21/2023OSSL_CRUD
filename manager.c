03c8493f35295a446be2a6d9d9e608cf964f1e95
SionLee21 <lso1030@handong.ac.kr>
2023-04-24 01:43:15 +0900

    saveData함수 구현

--- manager.c
+++ manager.c
@@ -33,30 +33,24 @@ int selectDataNo(Product *p, int count){
     getchar();
     return no;
 }
-
 //배열데이터를 파일에 저장하는 함수
 void saveData(Product p[], int count){
-	FILE* fp;
+    FILE* fp;
 
-	//중량 가격 제품명
-	fp= fopen("product.txt","wt");
-	
-	
-	fclose(fp);
-	printf("저장됨!\n");
+    // 중량 가격 제품명 형식으로 파일에 데이터 쓰기
+    fp = fopen("product.txt", "wt");
+    for(int i=0; i<count; i++){
+        if(p[i].weight == -1 || p[i].price == -1) continue;
+        fprintf(fp, "%d %d %s\n", p[i].weight, p[i].price, p[i].name);
+    }
+    fclose(fp);
+
+    printf("저장됨!\n");
 }
 
-
-//파일에서 데이터 불러오는 함수
 int loadData(Product *p){
-	int count=0;
-	FILE*fp;
-
-	//파일 내용을 읽어와서 배열에 값 추가하기
-
-
-
-
-	printf("=> 로딩 성공!\n");
-	return count;
+    int count = 0;
+    FILE* fp;
+    return count;
 }
+
